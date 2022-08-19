941e12eb94fe937a06cbf829941a90f00cef1417
DongS-K <ehdtjeo2117@gmail.com>
2022-08-19 15:10:56 +0900

    summernote
    community, postinfo, write : </button> -> </a>
    write : summernote 추가
    base : summernote 스크립트 추가
    custom.js
    : window.onload -> document.addEventListener
    : summernote 추가(UI만)

--- static/js/custom.js
+++ static/js/custom.js
@@ -13,7 +13,34 @@ function reviewTextEdit(riviewClass){
         }
     }
 }
-window.onload = function(){
+document.addEventListener("DOMContentLoaded", function(){
     reviewTextEdit("reviewTextInMain")
-}
+
+    $('#summernote').summernote({
+        placeholder: '내용을 입력해주세요.',
+        tabsize: 10,
+        height: 500,
+        minHeight: 500,             // set minimum height of editor
+        maxHeight: 500,             // set maximum height of editor
+        lang: 'ko-KR',
+        focus: true,
+        toolbar: [
+            ['fontname', ['fontname']],
+            ['fontsize', ['fontsize']],
+            ['style', ['bold', 'italic', 'underline','strikethrough', 'clear']],
+            ['color', ['forecolor','color']],
+            ['table', ['table']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['height', ['height']],
+            ['insert',['picture','link','video']],
+            ['view', ['fullscreen', 'help']]
+          ],
+        fontNames: ['Arial', 'Arial Black', 'Comic Sans MS', 'Courier New','맑은 고딕','궁서','굴림체','굴림','돋움체','바탕체'],
+        fontSizes: ['8','9','10','11','12','14','16','18','20','22','24','28','30','36','50','72']
+
+    });
+    $('.note-resizebar').css('display','none');
+    $('.dropdown-toggle').dropdown()
+
+});
 //2번째 슬라이드부터 적용이 제대로 되지 않으므로 임시로 첫번째 슬라이드만 적용
