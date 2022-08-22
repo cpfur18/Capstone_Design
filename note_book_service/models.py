b36cb093250ee1851419de66b75892fb7b5cc2d5
이강석 <cpfur18@gmail.com>
2022-08-23 02:10:47 +0900

    Fix : 오류 수정 및 DB저장 코드 추가

--- note_book_service/models.py
+++ note_book_service/models.py
@@ -7,8 +7,8 @@ class Prod(models.Model):
     prod_name = models.CharField(max_length=100)
     prod_price = models.CharField(max_length=100)
     prod_reg_date = models.CharField(max_length=100)
-    prod_review_grade = models.CharField(max_length=30, null=True)
-    prod_review_count = models.IntegerField(null=True)
+    # prod_review_grade = models.CharField(max_length=30, null=True)
+    # prod_review_count = models.IntegerField(null=True)
 
     def __str__(self):
         return self.prod_name
