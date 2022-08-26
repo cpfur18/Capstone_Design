273999cfd04058b604fe3be610848a5b1f36ff29
wnsgml7267 <dlwns7267@naver.com>
2022-08-26 17:00:33 +0900

    write.py -> community.py 클라이언트/서버 데이터 송수신

--- board/views.py
+++ board/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def getpostwrite(request):
+    title1 = request.POST.get("title")
+    content1 = request.POST.get("content")
+    context = { 'posttitle':title1, 'postcontent':content1}
+    return render(request, "note_book_service/community.html", context)
--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from note_book_service import views
-
+import board.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('note_book_service.urls')),
     path('', include('sign.urls')),
+    #path('abc/', board.views.abc),
+    path('getpost/write', board.views.getpostwrite),
     path('', views.index, name='index'),
 ]
