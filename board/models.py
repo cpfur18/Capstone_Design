fd83f6aa6d94bd3e4a1f4a5cef30d661df89e03f
wnsgml7267 <dlwns7267@naver.com>
2022-08-27 18:19:11 +0900

    게시판 모델 생성 + DB 연동

--- board/models.py
+++ board/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
-# Create your models here.
+#from django.utils import timezone
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    contents = models.TextField()
+    #create_date = models.DateTimeField(default=timezone.now, help_text="날짜 및 시간")
+    create_date = models.DateTimeField(auto_now_add=True)
--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'board',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                
             ],
         },
     },
