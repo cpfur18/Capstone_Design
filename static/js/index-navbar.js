fff568cc6ea03565420b079ba853386816f5214c
DongS-K <ehdtjeo2117@gmail.com>
2022-09-03 21:27:43 +0900

    잘못 푸쉬한거 복구

--- static/js/index-navbar.js
+++ static/js/index-navbar.js
@@ -18,29 +18,4 @@ document.addEventListener("DOMContentLoaded", function(){
         '        </table>' +
         '    </section>' +
     $('#nv').after(index_nav)
-
-    $(window).scroll(function() {
-        var scrollHeight = $(window).scrollTop() + $(window).height();
-        var documentHeight = $(document).height();
-
-        if (scrollHeight + 300 >= documentHeight){
-                $.ajax({
-                url: "/rest/1/pages/245", // 클라이언트가 HTTP 요청을 보낼 서버의 URL 주소
-                data: { name: "홍길동" },  // HTTP 요청과 함께 서버로 보낼 데이터
-                method: "GET",   // HTTP 요청 메소드(GET, POST 등)
-                dataType: "json" // 서버에서 보내줄 데이터의 타입
-            })
-        }
-    });
-    /*
-    var infinite = new Waypoint.Infinite({
-        element: $('.infinite-container')[0],
-        offset: 'bottom-in-view',
-        onBeforePageLoad: function (){
-            $('.spinner-border').show();
-        },
-        onAfterPageLoad: function (){
-            $('.spinner-border').hide();
-        }
-    })*/
 });
