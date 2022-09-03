31994e4fb34d1b182cc23bbd776a7557eca0d41a
이강석 <cpfur18@gmail.com>
2022-09-04 02:06:55 +0900

    REFACTOR : 더미코드 삭제

--- note_book_service/urls.py
+++ note_book_service/urls.py
@@ -1,7 +1,5 @@
-from django.contrib import admin
 from django.urls import path, include
 from note_book_service import views
-import board.views
 app_name = 'main'
 
 urlpatterns = [
@@ -10,7 +8,4 @@ urlpatterns = [
     path('recommend/', views.cmd, name='recommend'),
     path('recommend/result/', views.recr, name='rec_result'),
     path('recommend/result/<prod_id>', views.product, name='rec_result_info'),
-
-    # # path('expolre/<prod_id>/', views.expolre, name=''),
-    # path('search/', views.SearchFormView.as_view(), name='search'),
 ]
