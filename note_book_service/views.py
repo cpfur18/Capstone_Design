e4c7bae538fd9c45f5d6d09952d36f399eb7e6e6
이강석 <cpfur18@gmail.com>
2022-09-03 22:35:08 +0900

    FIX : list 없을 시 에러 해결 Error : list index out of range

--- note_book_service/views.py
+++ note_book_service/views.py
@@ -34,11 +34,23 @@ def index(request):
     paginator = Paginator(Prod_id_list, 5)  # 페이지당 5개씩 보여주기
     page_obj = paginator.get_page(1)
 
-    spec_tags_1 = page_obj.object_list[0].tags.values('prod', 'name')
-    spec_tags_2 = page_obj.object_list[1].tags.values('prod', 'name')
-    spec_tags_3 = page_obj.object_list[2].tags.values('prod', 'name')
-    spec_tags_4 = page_obj.object_list[3].tags.values('prod', 'name')
-    spec_tags_5 = page_obj.object_list[4].tags.values('prod', 'name')
+    page_count = len(page_obj.object_list)
+    spec_tags_1 = None
+    spec_tags_2 = None
+    spec_tags_3 = None
+    spec_tags_4 = None
+    spec_tags_5 = None
+
+    if page_count == 1:
+        spec_tags_1 = page_obj.object_list[0].tags.values('prod', 'name')
+    if page_count == 2:
+        spec_tags_2 = page_obj.object_list[1].tags.values('prod', 'name')
+    if page_count == 3:
+        spec_tags_3 = page_obj.object_list[2].tags.values('prod', 'name')
+    if page_count == 4:
+        spec_tags_4 = page_obj.object_list[3].tags.values('prod', 'name')
+    if page_count == 5:
+        spec_tags_5 = page_obj.object_list[4].tags.values('prod', 'name')
 
     context = {'Prod_list': page_obj,
                'spec_tags_1': spec_tags_1,
@@ -58,11 +70,23 @@ def getpage(request, page):
     paginator = Paginator(new_Prod_list, 5)  # 페이지당 5개씩 보여주기
     page_obj = paginator.get_page(page)
 
-    spec_tags_1 = page_obj.object_list[0].tags.values('prod', 'name')
-    spec_tags_2 = page_obj.object_list[1].tags.values('prod', 'name')
-    spec_tags_3 = page_obj.object_list[2].tags.values('prod', 'name')
-    spec_tags_4 = page_obj.object_list[3].tags.values('prod', 'name')
-    spec_tags_5 = page_obj.object_list[4].tags.values('prod', 'name')
+    page_count = len(page_obj.object_list)
+    spec_tags_1 = None
+    spec_tags_2 = None
+    spec_tags_3 = None
+    spec_tags_4 = None
+    spec_tags_5 = None
+
+    if page_count == 1:
+        spec_tags_1 = page_obj.object_list[0].tags.values('prod', 'name')
+    if page_count == 2:
+        spec_tags_2 = page_obj.object_list[1].tags.values('prod', 'name')
+    if page_count == 3:
+        spec_tags_3 = page_obj.object_list[2].tags.values('prod', 'name')
+    if page_count == 4:
+        spec_tags_4 = page_obj.object_list[3].tags.values('prod', 'name')
+    if page_count == 5:
+        spec_tags_5 = page_obj.object_list[4].tags.values('prod', 'name')
 
     context = {'Prod_list': page_obj,
                'spec_tags_1': spec_tags_1,
@@ -118,39 +142,48 @@ def recr(request):
     disp_color2 = request.POST['디스플레이_색감']
     disp_size = request.POST['디스플레이_크기']
 
-    print(purpose)
-    print(wight)
-    print(price)
-    print(as_)
-    # print(disp_media)
-    print(disp_color2)
-    print(disp_size)
+    # print(purpose)
+    # print(wight)
+    # print(price)
+    # print(as_)
+    # # print(disp_media)
+    # print(disp_color2)
+    # print(disp_size)
 
     recr_2 = check_recr.recr_123()
     id = recr_2.check_purpose(purpose)
-    print(id.values('prod_id'))
     id = recr_2.check_price(id, price)
-    print(id.values('prod_id'))
     # id = recr_2.check_weight(id, wight)
     id = recr_2.check_disp_size(id, disp_size)
-    print(id.values('prod_id'))
     id = recr_2.check_disp_color(id, disp_color2)
-    print(id.values('prod_id'))
     id = recr_2.check_as(id, as_)
-    print(id.values('prod_id'))
+
 
     recr_Prod_list = Prod.objects.filter(prod_id__in=id).order_by('-prod_id')
     # recr_Prod_list_spec = Prod_property.objects.filter(prod_id__in=recr_Prod_list).order_by('option_id')
+    print(recr_Prod_list)
 
     paginator = Paginator(recr_Prod_list, 5)  # 페이지당 5개씩 보여주기
     page_obj = paginator.get_page(5)
     # page_obj = paginator.get_page(page)
 
-    spec_tags_1 = page_obj.object_list[0].tags.values('prod', 'name')
-    spec_tags_2 = page_obj.object_list[1].tags.values('prod', 'name')
-    spec_tags_3 = page_obj.object_list[2].tags.values('prod', 'name')
-    spec_tags_4 = page_obj.object_list[3].tags.values('prod', 'name')
-    spec_tags_5 = page_obj.object_list[4].tags.values('prod', 'name')
+    page_count = len(page_obj.object_list)
+    spec_tags_1 = None
+    spec_tags_2 = None
+    spec_tags_3 = None
+    spec_tags_4 = None
+    spec_tags_5 = None
+
+    if page_count == 1:
+        spec_tags_1 = page_obj.object_list[0].tags.values('prod', 'name')
+    if page_count == 2:
+        spec_tags_2 = page_obj.object_list[1].tags.values('prod', 'name')
+    if page_count == 3:
+        spec_tags_3 = page_obj.object_list[2].tags.values('prod', 'name')
+    if page_count == 4:
+        spec_tags_4 = page_obj.object_list[3].tags.values('prod', 'name')
+    if page_count == 5:
+        spec_tags_5 = page_obj.object_list[4].tags.values('prod', 'name')
 
     context = {'Prod_list': page_obj,
                'spec_tags_1': spec_tags_1,
